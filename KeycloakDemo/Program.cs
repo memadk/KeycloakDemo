042f1f037750dd196895bf05f207fe334a498d58
User <User@WINDEV2208EVAL>
2022-11-23 15:50:18 +0100

    Add project files.

---
+++ KeycloakDemo/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web.UI;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect("keycloak", options =>
+    {
+        options.SignInScheme = "Identity.External";
+        options.Authority = "http://localhost:8080/auth/realms/Test";
+        options.ClientId = "test-client";
+        options.RequireHttpsMetadata = false;
+        options.SaveTokens = true;
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.ClientSecret = "4o4IzKdWTaJ4ir6iCdKjVXDhWoDiq3Zo";
+        options.MetadataAddress = "http://localhost:8080/auth/realms/Test/.well-known/openid-configuration";
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ResponseType = OpenIdConnectResponseType.Code;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "name",
+            RoleClaimType = ClaimTypes.Role,
+            ValidateIssuer = true
+        };
+    });
+
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
